cb0fa3df0ea120c5f4d790bcec50bb2f287cf1bc
Endryll <endryll.silverio.dantas@gmail.com>
2023-04-14 15:09:57 -0300

    Programa em phyton que lê 5 números e informe a soma e a média dos números.

---
+++ exercicios_de_repetição_8.py
@@ -0,0 +1,10 @@
+primeiro_numero=float(input("digite o 1º numero: "))
+segundo_numero=float(input("digite o 2º numero: "))
+terceiro_numero=float(input("digite o 3º numero: "))
+quarto_numero=float(input("digite o 4º numero: "))
+quinto_numero=float(input("digite o 5º numero: "))
+
+soma = primeiro_numero + segundo_numero + terceiro_numero + quarto_numero + quinto_numero
+
+print("soma:",soma,)
+print("média:",soma/5)
