4203f919c31799204ab87780eb8dea94d475e32b
Endryll <endryll.silverio.dantas@gmail.com>
2023-04-14 08:51:05 -0300

    Programa em phyton que lê três vetores com 10, Gera um terceiro vetor de 20 elementos, cujos valores são compostos pelos elementos intercalados dos três outros vetores.

--- exercicios_de_lista_11.py
+++ exercicios_de_lista_11.py
@@ -1,19 +1,23 @@
 vetor_1 = []
 vetor_2 = []
-
+vetor_3 = []
 
 for i in range(10):
     valor = int(input("Digite um valor para o primeiro vetor: "))
     vetor_1.append(valor)
     valor = int(input("Digite um valor para o segundo vetor: "))
     vetor_2.append(valor)
+    valor = int(input("Digite um valor para o terceiro vetor: "))
+    vetor_3.append(valor)
 
-vetor_3 = []
+intercalado = []
 
 for i in range(10):
-    vetor_3.append(vetor_1[i])
-    vetor_3.append(vetor_2[i])
+    intercalado.append(vetor_1[i])
+    intercalado.append(vetor_2[i])
+    intercalado.append(vetor_3[i])
 
 print("Primeiro Vetor:", vetor_1)
 print("Segundo Vetor:", vetor_2)
-print("Terceiro vetor intercalado:", vetor_3)
+print("Terceiro Vetor:", vetor_3)
+print("Vetores Intercalada:", intercalado)
